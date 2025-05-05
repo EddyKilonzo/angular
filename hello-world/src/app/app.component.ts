e58a2203640fa627ee5b84cee438495819f7955d
Eddy Kilonzo <eddymax3715@gmail.com>
2025-05-05 19:22:46 +0300

    user and admin

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AdminComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular19 tutorial';
 }
